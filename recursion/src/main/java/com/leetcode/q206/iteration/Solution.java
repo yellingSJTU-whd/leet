b8bbd54560864f71e0bd4375401d85575865fbd4
eYyoc <haode_wang@sjtu.edu.cn>
2023-10-17 15:02:49 +0800

    iterate solution for question 206

---
+++ recursion/src/main/java/com/leetcode/q206/iteration/Solution.java
@@ -0,0 +1,32 @@
+package com.leetcode.q206.iteration;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
