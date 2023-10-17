2da090bf70a6ecca542bbf5c661a810cf03bd0ef
eYyoc <haode_wang@sjtu.edu.cn>
2023-10-17 12:12:28 +0800

    iteration solution for question 21

---
+++ recursion/src/main/java/com/leetcode/q21/iteration/Solution.java
@@ -0,0 +1,43 @@
+package com.leetcode.q21.iteration;
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            if (l1 != null) {
+                carry += l1.val;
+            }
+            if (l2 != null) {
+                carry += l2.val;
+            }
+            curr = curr.next = new ListNode(carry % 10);
+            carry /= 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return dummy.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
