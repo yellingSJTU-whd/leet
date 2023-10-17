931be9f8e30fc82059c256493c0f1a1066fb9c52
eYyoc <haode_wang@sjtu.edu.cn>
2023-10-17 15:03:11 +0800

    recursive solution for question 206

---
+++ recursion/src/main/java/com/leetcode/q206/recursion/Solution.java
@@ -0,0 +1,30 @@
+package com.leetcode.q206.recursion;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
