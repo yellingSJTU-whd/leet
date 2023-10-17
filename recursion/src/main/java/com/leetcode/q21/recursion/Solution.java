a6eb38bb1a1a7381b41a26e1f7f16cc5ce190833
eYyoc <haode_wang@sjtu.edu.cn>
2023-10-17 12:11:27 +0800

    recursion solution for question 21

---
+++ recursion/src/main/java/com/leetcode/q21/recursion/Solution.java
@@ -0,0 +1,42 @@
+package com.leetcode.q21.recursion;
+
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return doAdd(l1, l2, 0);
+    }
+
+    private ListNode doAdd(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null) {
+            return carry == 0 ? null : new ListNode(carry);
+        }
+        if (l1 == null) {
+            l1 = l2;
+            l2 = null;
+        }
+        if (l2 == null && carry == 0) {
+            return l1;
+        }
+        carry += l1.val + (l2 == null ? 0 : l2.val);
+        l1.val = carry % 10;
+        l1.next = doAdd(l1.next, (l2 == null ? null : l2.next), carry / 10);
+        return l1;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
