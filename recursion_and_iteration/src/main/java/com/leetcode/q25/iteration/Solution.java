af591ffe0f838496926f7959079662ced790b2ea
eYyoc <haode_wang@sjtu.edu.cn>
2023-10-19 11:05:51 +0800

    native iteration solution for question 25

---
+++ recursion_and_iteration/src/main/java/com/leetcode/q25/iteration/Solution.java
@@ -0,0 +1,63 @@
+package com.leetcode.q25.iteration;
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null || k == 1) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(-1, head), ptr = dummy;
+        ListNode pre = null, curr = head;
+        int count = 0;
+        while (curr != null) {
+            ListNode next = curr.next;
+            if (count != 0) {
+                curr.next = pre;
+            }
+            pre = curr;
+            curr = next;
+
+            count = (count + 1) % k;
+            if (count == 0) {
+                ListNode tmp = ptr.next;
+                ptr.next = pre;
+                ptr = tmp;
+                ptr.next = curr;
+            }
+        }
+
+        if (count != 0) {
+            ptr.next.next = null;
+            reverseList(pre);
+        }
+        return dummy.next;
+    }
+
+    private void reverseList(ListNode head) {
+        ListNode pre = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
