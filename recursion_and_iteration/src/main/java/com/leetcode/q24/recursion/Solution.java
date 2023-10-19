64ea0d3807792c770414a0c1afac13059515918f
eYyoc <haode_wang@sjtu.edu.cn>
2023-10-19 15:50:21 +0800

    recursive solution for question 24

---
+++ recursion_and_iteration/src/main/java/com/leetcode/q24/recursion/Solution.java
@@ -0,0 +1,31 @@
+package com.leetcode.q24.recursion;
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
